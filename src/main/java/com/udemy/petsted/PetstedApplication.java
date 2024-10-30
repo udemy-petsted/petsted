6c3336230b17e9e9700934b9434ac757123223ec
sangwopark19 <sangwopark19@gmail.com>
2024-10-30 16:40:17 +0900

    fix: @CreateDate, LastModifiedDate가 적용되기 위한 설정

--- src/main/java/com/udemy/petsted/PetstedApplication.java
+++ src/main/java/com/udemy/petsted/PetstedApplication.java
@@ -2,12 +2,14 @@ package com.udemy.petsted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PetstedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetstedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetstedApplication.class, args);
+    }
 
 }
--- src/main/java/com/udemy/petsted/auth/entity/User.java
+++ src/main/java/com/udemy/petsted/auth/entity/User.java
@@ -2,6 +2,7 @@ package com.udemy.petsted.auth.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "users")
@@ -22,6 +24,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 @NoArgsConstructor
 @Getter
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
