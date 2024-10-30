b503a143520dd7c3a2c739a37f35ba417500e1ed
sangwopark19 <sangwopark19@gmail.com>
2024-10-30 16:51:08 +0900

    feat: users insert SQL 생성 앱 실행시 자동으로 test 데이터를 생성한다

---
+++ src/main/resources/SQL/users.sql
@@ -0,0 +1,5 @@
+insert INTO users (username, password, email, has_pet, profile_url, phone_number, birth_date,
+                   region, manner, created_date, updated_date)
+values ( 'test1', 'test1', 'test1', true, 'test1'
+       , 'test1', 'test1', 'test1', 0.0
+       , NOW(), NOW());
