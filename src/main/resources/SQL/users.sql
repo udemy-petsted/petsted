24ab3d94223a193b452c78068198414af84727df
sangwopark19 <sangwopark19@gmail.com>
2024-10-30 17:19:42 +0900

    feat: user.sql 테스트를 위해 값 변경 유니크값이 중복되어 변경했다

--- src/main/resources/SQL/users.sql
+++ src/main/resources/SQL/users.sql
@@ -1,5 +1,5 @@
 insert INTO users (username, password, email, has_pet, profile_url, phone_number, birth_date,
                    region, manner, created_date, updated_date)
-values ( 'test1', 'test1', 'test1', true, 'test1'
-       , 'test1', 'test1', 'test1', 0.0
+values ( 'admin', 'admin', 'admin', true, 'admin'
+       , 'admin', 'admin', 'admin', 0.0
        , NOW(), NOW());
