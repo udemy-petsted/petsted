19fa6621ddc448d52cdbedcf765e9545517473c5
sangwopark19 <sangwopark19@gmail.com>
2024-10-31 19:01:30 +0900

    feat: findByUsername 메서드 추가

--- src/main/java/com/udemy/petsted/auth/repository/UserRepository.java
+++ src/main/java/com/udemy/petsted/auth/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.udemy.petsted.auth.repository;
 
 import com.udemy.petsted.auth.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
 }
