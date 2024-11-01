50134c98610b8393f937f72a6a7ee79bf4967e2e
sangwopark19 <sangwopark19@gmail.com>
2024-11-01 16:54:36 +0900

    feat: DataLoader 내부 로직 DummyUserDataList로 분리
    db가 분리된 테스트 환경에서도 더미데이터를 활용할 수 있게 분리했다

--- src/main/java/com/udemy/petsted/config/DataLoader.java
+++ src/main/java/com/udemy/petsted/config/DataLoader.java
@@ -1,8 +1,7 @@
 package com.udemy.petsted.config;
 
-import com.udemy.petsted.auth.entity.User;
 import com.udemy.petsted.auth.repository.UserRepository;
-import net.datafaker.Faker;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,27 +10,10 @@ import org.springframework.context.annotation.Configuration;
 public class DataLoader {
 
     @Bean
-    public CommandLineRunner loadData(UserRepository userRepository) {
+    public CommandLineRunner loadData(UserRepository userRepository,
+        DummyUserDataList dummyUserDataList) {
         return args -> {
-            Faker faker = new Faker();
-            for (int i = 0; i < 100; i++) {
-                User user = User.builder()
-                    .nickname("dummy" + i)
-                    .username("dummy" + i)
-                    .password(faker.internet().password())
-                    .email(faker.internet().emailAddress())
-                    .hasPet(false)
-                    .profileUrl(faker.internet().url())
-                    .phoneNumber(faker.phoneNumber().cellPhone())
-                    .birthDate(faker.date().birthday(1, 99, "YYYY-MM-DD"))
-                    .region(faker.address().country())
-                    // 소수점 두 자리에서 반올림한다
-                    .manner(Math.round(faker.random().nextDouble(0.0, 99.9) * 10) / 10.0)
-                    .build();
-                Double j = Double.longBitsToDouble(
-                    Math.round(faker.random().nextDouble(0.0, 99.9)));
-                userRepository.save(user);
-            }
+            userRepository.saveAll(dummyUserDataList.create(100));
         };
     }
 }
---
+++ src/main/java/com/udemy/petsted/config/DummyUserDataList.java
@@ -0,0 +1,35 @@
+package com.udemy.petsted.config;
+
+import com.udemy.petsted.auth.entity.User;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import net.datafaker.Faker;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DummyUserDataList {
+
+    public List<User> create(int count) {
+        Faker faker = new Faker();
+
+        return IntStream.range(0, count)
+            .mapToObj(i ->
+                User.builder()
+                    .nickname("dummy" + i)
+                    .username("dummy" + i)
+                    .password(faker.internet().password())
+                    .email(faker.internet().emailAddress())
+                    .hasPet(false)
+                    .profileUrl(faker.internet().url())
+                    .phoneNumber(faker.phoneNumber().cellPhone())
+                    .birthDate(faker.date().birthday(1, 99, "YYYY-MM-DD"))
+                    .region(faker.address().country())
+                    // 소수점 두 자리에서 반올림한다
+                    .manner(Math.round(faker.random().nextDouble(0.0, 99.9) * 10) / 10.0)
+                    .build()
+            ).toList();
+
+    }
+}
