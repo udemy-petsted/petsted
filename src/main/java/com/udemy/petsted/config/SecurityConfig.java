4b19513022092abaec3023f07c0a7d9742cb8bcb
sangwopark19 <sangwopark19@gmail.com>
2024-11-04 18:05:25 +0900

    feat: PasswordEncoder 빈 추가 BCryptPasswordEncoder를 사용한다

--- src/main/java/com/udemy/petsted/config/SecurityConfig.java
+++ src/main/java/com/udemy/petsted/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.udemy.petsted.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
@@ -11,6 +14,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@Log4j2
 public class SecurityConfig {
 
     @Bean
@@ -23,5 +27,10 @@ public class SecurityConfig {
                 XFrameOptionsMode.SAMEORIGIN)));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
