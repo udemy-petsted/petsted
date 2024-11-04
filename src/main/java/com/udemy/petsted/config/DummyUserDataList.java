efff1899f85630c92602d5039a1413f329e854d6
sangwopark19 <sangwopark19@gmail.com>
2024-11-04 18:30:16 +0900

    feat: 더미데이터 생성 클래스 PasswordEncoder 추가

--- src/main/java/com/udemy/petsted/config/DummyUserDataList.java
+++ src/main/java/com/udemy/petsted/config/DummyUserDataList.java
@@ -6,9 +6,17 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 import net.datafaker.Faker;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-
+/**
+ * 스프링 컨텍스트를 사용하지 않고 JPA 기능만 사용하는 @DataJpaTest에서도 사용하기 위해
+ * 컴포넌트로 지정하지 않고 메서드들을 static으로 지정했다
+ */
 public class DummyUserDataList {
+
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public static List<User> create(int count) {
         Faker faker = new Faker();
@@ -18,7 +26,7 @@ public class DummyUserDataList {
                 User.builder()
                     .nickname("dummy" + i)
                     .username("dummy" + i)
-                    .password(faker.internet().password())
+                    .password(passwordEncoder.encode("dummy"))
                     .email(faker.internet().emailAddress())
                     .hasPet(false)
                     .profileUrl(faker.internet().url())
